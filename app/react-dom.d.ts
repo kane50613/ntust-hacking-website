288fa4e898165070f8a0713f464d30f732905172
Kane Wang <kane@yeecord.com>
2024-11-10 03:05:02 +0800

    feat: try switch to server.edge import

--- app/react-dom.d.ts
+++ app/react-dom.d.ts
@@ -1,4 +1,4 @@
-declare module "react-dom/server.browser" {
+declare module "react-dom/server.edge" {
   import { renderToReadableStream } from "react-dom/server";
 
   export { renderToReadableStream };
