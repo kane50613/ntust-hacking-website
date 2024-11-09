3924a41930c25aa04d46b45c8c817d9b5c3bb209
Kane Wang <kane@yeecord.com>
2024-11-10 03:14:06 +0800

    feat: use empty array for fallback

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -36,7 +36,7 @@ export default function Index({
   return (
     <div className="flex flex-col items-center justify-center">
       <Hero isRed={isRed} />
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<Events eventRecords={[]} />}>
         <Await resolve={eventRecords} errorElement={<AsyncError />}>
           {(eventRecords) => <Events eventRecords={eventRecords} />}
         </Await>
