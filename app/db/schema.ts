2f12483eb7fb4cfb51ed966f4c60fdf88f7c2bc5
Kane Wang <kane@yeecord.com>
2024-11-25 00:35:32 +0800

    feat: finish schema

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -1,4 +1,11 @@
+import { relations } from "drizzle-orm";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+const createdAt = integer({
+  mode: "timestamp",
+})
+  .notNull()
+  .$defaultFn(() => new Date());
 
 export const events = sqliteTable("events", {
   eventId: integer().primaryKey({ autoIncrement: true }),
@@ -7,6 +14,7 @@ export const events = sqliteTable("events", {
   date: integer({
     mode: "timestamp",
   }),
+  createdAt,
 });
 
 export const eventLinks = sqliteTable("eventLinks", {
@@ -14,4 +22,56 @@ export const eventLinks = sqliteTable("eventLinks", {
   eventId: integer().notNull(),
   link: text().notNull(),
   label: text(),
+  createdAt,
 });
+
+export const users = sqliteTable("users", {
+  userId: integer().primaryKey({ autoIncrement: true }),
+  name: text().notNull(),
+  email: text().unique(),
+  createdAt,
+});
+
+export const enrolls = sqliteTable("enrolls", {
+  enrollId: integer().primaryKey({ autoIncrement: true }),
+  userId: integer()
+    .notNull()
+    .references(() => users.userId),
+  eventId: integer().notNull(),
+  createdAt,
+});
+
+export const feedbacks = sqliteTable("feedbacks", {
+  feedbackId: integer().primaryKey({ autoIncrement: true }),
+  eventId: integer()
+    .notNull()
+    .references(() => events.eventId),
+  userId: integer()
+    .notNull()
+    .references(() => users.userId),
+  comment: text(),
+  createdAt,
+});
+
+export const eventRelations = relations(events, ({ many }) => ({
+  links: many(eventLinks),
+  enrolls: many(enrolls),
+  feedbacks: many(feedbacks),
+}));
+
+export const userRelations = relations(users, ({ many }) => ({
+  enrolls: many(enrolls),
+}));
+
+export const enrollRelations = relations(enrolls, ({ one }) => ({
+  event: one(events),
+}));
+
+export const feedbackRelations = relations(feedbacks, ({ one }) => ({
+  event: one(events),
+  user: one(users),
+}));
+
+export const eventLinkRelations = relations(eventLinks, ({ one }) => ({
+  event: one(events),
+}));
