ba068e9393e1f16db07bea202ba033319e70f1a8
kane50613 <kane@yeecord.com>
2024-11-26 18:43:55 +0800

    fix: client action update order

--- app/routes/enroll.tsx
+++ app/routes/enroll.tsx
@@ -1,7 +1,8 @@
-import { toast } from "sonner";
+import { parse } from "devalue";
 import { z } from "zod";
 import { db } from "~/db";
 import { enrolls } from "~/db/schema";
+import { clientActionToast } from "~/lib/client-action-toast";
 import type { Route } from "~/routes/+types/enroll";
 import { getSessionFromRequest } from "~/session";
 
@@ -12,7 +13,7 @@ const enrollSchema = z.object({
 export type EnrollPayload = z.infer<typeof enrollSchema>;
 
 export async function action({ request }: Route.LoaderArgs) {
-  const { eventId } = enrollSchema.parse(await request.json());
+  const { eventId } = enrollSchema.parse(parse(await request.text()));
 
   const session = await getSessionFromRequest(request);
 
@@ -29,7 +30,7 @@ export async function action({ request }: Route.LoaderArgs) {
 }
 
 export async function clientAction({ serverAction }: Route.ClientActionArgs) {
-  toast.promise(serverAction(), {
+  return clientActionToast(serverAction(), {
     loading: "正在報名...",
     success: "報名成功",
     error(error: unknown) {
